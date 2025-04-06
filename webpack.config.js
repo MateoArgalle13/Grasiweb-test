fd58bcb07d3af0751dd19599d1c93fcfaca06135
Mateo Argalle <mateo.argalle@gmail.com>
2025-04-06 15:50:11 -0500

    config webpack e install package

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,54 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'main.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'js/[name].[contenthash].js',
+        assetModuleFilename: 'assets/[hash][ext][query]',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            minify: { collapseWhitespace: true },
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].[contenthash].css',
+        }),
+        new CleanWebpackPlugin(),
     ],
-  },
-  mode: 'development',
+    optimization: {
+        splitChunks: { chunks: 'all' },
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
+    devServer: {
+        static: path.join(__dirname, 'public'),
+        compress: true,
+        port: 3000,
+        hot: true,
+        open: true,
+    },
+    mode: process.env.NODE_ENV || 'development',
+    devtool: 'source-map',
 };
